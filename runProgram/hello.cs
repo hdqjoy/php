14e8095d4de7577c88ab5a9f06471b3d6db04974
deqiao.huang <deqiao.huang@cdd100.com>
2016-12-20 00:30:52 +0800

    run program

---
+++ runProgram/hello.cs
@@ -0,0 +1,10 @@
+using System;
+// A "Hello World!" program in C#
+namespace HelloWorld
+{
+    class HelloWorld{
+        public static void Main(){
+            Console.WriteLine("HelloWorld");
+        }
+    }
+}
